d7e08ae390ed325616845ecdd8138112864ac49c
Luís Augusto Ferreti Pinheiro <laf.pinheiro@gmail.com>
2020-07-15 21:16:44 +0100

    Primeira versão concluída.

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '85d53a1b13a08c1ee38902e6950805d2',
+    expiresIn: '1d',
+  },
+};
---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,19 @@
+import { EntityRepository, Repository } from 'typeorm';
+import Appointment from '../models/Appointment';
+
+interface CreateRepositoryDTO {
+  provider: string;
+  date: Date;
+}
+
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointment = await this.findOne({
+      where: { date },
+    });
+    return findAppointment || null;
+  }
+}
+
+export default AppointmentsRepository;
